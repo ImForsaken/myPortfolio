8b5641b063e88de8c6b10943c78ee69c71cd07fa
Kevin Herbst <kevin-herbst1993@web.de>
2023-04-10 18:57:17 +0200

    added mobile nav&logic&responsiveUpdates

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -6,41 +12,76 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./contact-form.component.scss'],
 })
 export class ContactFormComponent {
-  @ViewChild('myForm') myForm!: ElementRef;
-  @ViewChild('nameField') nameField!: ElementRef;
-  @ViewChild('emailField') emailField!: ElementRef;
-  @ViewChild('messageField') messageField!: ElementRef;
-  @ViewChild('sendButton') sendButton!: ElementRef;
+  // @ViewChild('myForm') myForm!: ElementRef;
+  // @ViewChild('nameField') nameField!: ElementRef;
+  // @ViewChild('emailField') emailField!: ElementRef;
+  // @ViewChild('messageField') messageField!: ElementRef;
+  // @ViewChild('sendButton') sendButton!: ElementRef;
 
-  async sendMail() {
-    // action="https://herbst-kevin.de/send_mail.php"
-    console.log('sending mail', this.myForm);
-    let nameField = this.nameField.nativeElement;
-    let emailField = this.emailField.nativeElement;
-    let messageField = this.messageField.nativeElement;
-    let sendButton = this.sendButton.nativeElement;
+  // async sendMail() {
+  //   // action="https://herbst-kevin.de/send_mail.php"
+  //   console.log('sending mail', this.myForm);
+  //   let nameField = this.nameField.nativeElement;
+  //   let emailField = this.emailField.nativeElement;
+  //   let messageField = this.messageField.nativeElement;
+  //   let sendButton = this.sendButton.nativeElement;
 
-    nameField.disabled = true;
-    emailField.disabled = true;
-    messageField.disabled = true;
-    sendButton.disabled = true;
-    //Animation für absenden der Daten
+  //   nameField.disabled = true;
+  //   emailField.disabled = true;
+  //   messageField.disabled = true;
+  //   sendButton.disabled = true;
+  //   //Animation für absenden der Daten
 
-    let fd = new FormData();
-    fd.append('name', nameField.value);
-    fd.append('email', emailField.value);
-    fd.append('message', messageField.value);
+  //   let fd = new FormData();
+  //   fd.append('name', nameField.value);
+  //   fd.append('email', emailField.value);
+  //   fd.append('message', messageField.value);
 
-    // senden
-    await fetch('https://herbst-kevin.de/send_mail.php', {
-      method: 'POST',
-      body: fd,
+  //   // senden
+  //   await fetch('https://herbst-kevin.de/send_mail.php', {
+  //     method: 'POST',
+  //     body: fd,
+  //   });
+
+  //   //texte anzeigen nachricht gesendet
+  //   nameField.disabled = false;
+  //   emailField.disabled = false;
+  //   messageField.disabled = false;
+  //   sendButton.disabled = false;
+  // }
+
+  contactForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,4})+$/),
+        ],
+      ],
+      message: ['', Validators.required],
     });
+  }
 
-    //texte anzeigen nachricht gesendet
-    nameField.disabled = false;
-    emailField.disabled = false;
-    messageField.disabled = false;
-    sendButton.disabled = false;
+  get name() {
+    return this.contactForm.get('name') as FormControl;
+  }
+
+  get email() {
+    return this.contactForm.get('email') as FormControl;
+  }
+
+  get message() {
+    return this.contactForm.get('message') as FormControl;
+  }
+
+  submitForm() {
+    console.log(this.contactForm.value);
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  activeLinkIndex = -1;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const navLinks = document.querySelectorAll('.desktopNav a');
+    const mobileLinks = document.querySelectorAll('.mobileNavLinks a');
+    const checkBox = document.getElementById('menyAvPaa') as HTMLInputElement;
+
+    navLinks.forEach((link, index) => {
+      link.addEventListener('click', () => {
+        this.activeLinkIndex = index;
+      });
+    });
+
+    mobileLinks.forEach((link) => {
+      link.addEventListener('click', () => {
+        checkBox.checked = false;
+      });
+    });
+  }
+
+  isActive(index: number): boolean {
+    return this.activeLinkIndex === index;
+  }
+}
